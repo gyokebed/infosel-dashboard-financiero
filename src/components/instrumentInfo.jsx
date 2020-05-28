136e111a1faa528155dd68de31721611549fcb68
Gustavo Carrillo (Laptop) <g.yokebed@gmail.com>
2020-05-28 12:28:13 -0500

    Converted timestamp data to human readable format

--- src/components/instrumentInfo.jsx
+++ src/components/instrumentInfo.jsx
@@ -2,15 +2,21 @@ import React from "react";
 
 const InstrumentInfo = ({ data, currentInstrument }) => {
   const price = data.type === "trade" ? data.data[0].p : data.c;
-  const lastRefreshed = data.type === "trade" ? data.data[0].t : data.t;
+  let lastRefreshed = data.type === "trade" ? data.data[0].t : data.t * 1000;
 
-  console.log(price);
+  function convert(unixTimestamp) {
+    const dateObject = new Date(unixTimestamp);
+    const humanDateFormat = dateObject.toLocaleString(); //2019-12-9 10:30:15
+    return humanDateFormat;
+  }
+
+  lastRefreshed = convert(lastRefreshed);
 
   return (
     <React.Fragment>
       <div>{data ? currentInstrument : ""}</div>
       <div>Price: {data ? price : ""}</div>
-      <div>Last refreshed: {data ? lastRefreshed : ""}</div>
+      <div>Last trade: {data ? lastRefreshed : ""}</div>
     </React.Fragment>
   );
 };
