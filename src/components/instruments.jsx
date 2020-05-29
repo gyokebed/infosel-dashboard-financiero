b472fcd326a0c6fed46735bf7e275cf7b0afe82b
Gustavo Carrillo (Laptop) <g.yokebed@gmail.com>
2020-05-29 10:25:06 -0500

    Added chart with fake data

--- src/components/instruments.jsx
+++ src/components/instruments.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import Grid from "@material-ui/core/Grid";
 import InstrumentsTable from "./instrumentsTable";
 import InstrumentInfo from "./instrumentInfo";
+import InstrumentsChart from "./instrumentsChart";
 import PaginationContainer from "./common/pagination";
 import { paginate } from "../utils/paginate";
 
@@ -17,6 +18,7 @@ const Instruments = () => {
   const [realTimeData, setRealTimeData] = useState("");
   const [currentInstrument, setCurrentInstrument] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
+  const [monthtlyData, setMonthtlyData] = useState([]);
 
   // API call -> Retrieve Symbols from Finnhub
   useEffect(() => {
@@ -34,6 +36,23 @@ const Instruments = () => {
       setRealTimeData(instrumentDataResult.data);
       setInstrumentData(instrumentDataResult.data);
 
+      const result = await axios(
+        `https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=IBM&apikey=demo`
+      );
+
+      let monthtlyArrayData = [];
+      let monthtlyTimeSeries = Object.keys(result.data)[1];
+      for (let key in result.data[monthtlyTimeSeries])
+        monthtlyArrayData.push({
+          Date: key,
+          Open: result.data[monthtlyTimeSeries][key]["1. open"],
+          High: result.data[monthtlyTimeSeries][key]["2. high"],
+          Low: result.data[monthtlyTimeSeries][key]["3. low"],
+          Close: result.data[monthtlyTimeSeries][key]["4. close"],
+          // volume: result.data[monthtlyTimeSeries][key]["5. volume"],
+        });
+      setMonthtlyData(monthtlyArrayData.reverse());
+
       // New instance of websocket to connect real time data
 
       socket = new WebSocket(`wss://ws.finnhub.io?token=${token}`);
@@ -46,7 +65,7 @@ const Instruments = () => {
       // Listen for messages
       socket.addEventListener("message", function (event) {
         const receivedData = JSON.parse(event.data);
-        console.log("Message from server ", receivedData);
+        // console.log("Message from server ", receivedData);
         if (receivedData.type === "trade") setRealTimeData(receivedData);
       });
     }
@@ -89,27 +108,34 @@ const Instruments = () => {
   const { totalCount, filteredData } = getPagedData();
 
   return (
-    <Grid container spacing={3}>
-      <Grid item sm={6}>
-        <InstrumentsTable
-          data={instrumentData}
-          instruments={filteredData}
-          onClick={handleClick}
-        />
-        <PaginationContainer
-          itemsCount={totalCount}
-          pageSize={pageSize}
-          onPageChange={handlePageChange}
-        />
+    <React.Fragment>
+      <Grid container spacing={3}>
+        <Grid item sm={6}>
+          <InstrumentsTable
+            data={instrumentData}
+            instruments={filteredData}
+            onClick={handleClick}
+          />
+          <PaginationContainer
+            itemsCount={totalCount}
+            pageSize={pageSize}
+            onPageChange={handlePageChange}
+          />
+        </Grid>
+        <Grid item sm={6}>
+          <InstrumentInfo
+            realTimeData={realTimeData}
+            data={instrumentData}
+            currentInstrument={currentInstrument}
+          />
+        </Grid>
       </Grid>
-      <Grid item sm={6}>
-        <InstrumentInfo
-          realTimeData={realTimeData}
-          data={instrumentData}
-          currentInstrument={currentInstrument}
-        />
-      </Grid>
-    </Grid>
+
+      <InstrumentsChart
+        data={monthtlyData}
+        currentInstrument={currentInstrument}
+      />
+    </React.Fragment>
   );
 };
 
